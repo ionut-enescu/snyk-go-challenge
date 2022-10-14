634f90c682f8be0d99b0e8188b625df5be9aef65
Ionut Enescu <ionut.462.enescu@gmail.com>
2022-10-14 22:17:34 +0300

    some improvements

--- api/api.go
+++ api/api.go
@@ -12,48 +12,14 @@ import (
 	"net/http"
 	"os"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/Masterminds/semver/v3"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
-
-// IE: use log for logging instead of simple Println for extra features (i.e. timestamp)
-var errorLogger *log.Logger
-var debugLogger *log.Logger
-
-// IE: use a WaitGroup to process each recursive resolveDependencies() request asynchronously
-var wg sync.WaitGroup
-
-// IE: cache the last request for instant response on repeated identical requests
-var lastRequest map[string][]byte
-
-// IE: debug counter for start/end resolveDependencies()
-var goroutineCount WaitGroupCount
-
-var rootPkg *NpmPackageVersion
-
-var scannedPkgs map[string]*NpmPackageVersion
-
-func New() http.Handler {
-	// IE: use log for logging instead of fmt for extra features (i.e. timestamp)
-	errorLogger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-	debugLogger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-
-	// IE: in case we need to limit resource CPU Load
-	// runtime.GOMAXPROCS(runtime.NumCPU() * 0.75)
-	goroutineCount = WaitGroupCount{wg, 0}
-
-	router := mux.NewRouter()
-	router.Handle("/package/{package}/{version}", http.HandlerFunc(packageHandler))
-
-	// IE: cache the last request for instant response on repeated identical requests
-	lastRequest = make(map[string][]byte)
-
-	scannedPkgs = make(map[string]*NpmPackageVersion)
-	return router
-}
 
 type npmPackageMetaResponse struct {
 	Versions map[string]npmPackageResponse `json:"versions"`
@@ -85,9 +51,51 @@ type npmPackageResponse struct {
 }
 
 type NpmPackageVersion struct {
-	Name         string                        `json:"name"`
-	Version      string                        `json:"version"`
-	Dependencies map[string]*NpmPackageVersion `json:"dependencies"`
+	Name         string                        `json:"name" deepcopier:"field:Name"`
+	Version      string                        `json:"version"  deepcopier:"field:Version"`
+	Dependencies map[string]*NpmPackageVersion `json:"dependencies" deepcopier:"field:Dependencies"`
+	sync.RWMutex `deepcopier:"skip"`
+}
+
+// IE: use log for logging instead of simple Println for extra features (i.e. timestamp)
+var errorLogger *log.Logger
+var debugLogger *log.Logger
+
+// IE: use a WaitGroup to process each recursive resolveDependencies() request asynchronously
+var wg sync.WaitGroup
+
+// IE: cache the last request for instant response on repeated identical requests
+var lastRequest map[string][]byte
+
+// IE: debug counter for start/end resolveDependencies()
+var goroutineCount WaitGroupCount
+
+var rootPkg *NpmPackageVersion
+
+var scannedPkgs map[string]*NpmPackageVersion
+var scannedPkgsMutex = sync.RWMutex{}
+
+var copiedDeps map[string]string
+
+func New() http.Handler {
+	// IE: use log for logging instead of fmt for extra features (i.e. timestamp)
+	errorLogger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	debugLogger = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// IE: in case we need to limit resource CPU Load
+	// runtime.GOMAXPROCS(runtime.NumCPU() * 0.75)
+	goroutineCount = WaitGroupCount{}
+
+	router := mux.NewRouter()
+	router.Handle("/package/{package}/{version}", http.HandlerFunc(packageHandler))
+
+	// IE: cache the last request for instant response on repeated identical requests
+	lastRequest = make(map[string][]byte)
+
+	scannedPkgs = make(map[string]*NpmPackageVersion)
+	copiedDeps = make(map[string]string)
+
+	return router
 }
 
 func packageHandler(w http.ResponseWriter, r *http.Request) {
@@ -115,12 +123,21 @@ func packageHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// IE: NpmPackageVersion also has a 'version' attribute, should pass 'pkgVersion' into rootPkg
-		rootPkg = &NpmPackageVersion{Name: pkgName, Version: pkgVersion, Dependencies: map[string]*NpmPackageVersion{}}
+		newEmptyDeps := make(map[string]*NpmPackageVersion)
+		newEmptyDeps[uuid.NewString()] = nil
+		rootPkg = &NpmPackageVersion{Name: pkgName, Version: pkgVersion, Dependencies: newEmptyDeps}
 
 		// IE: send task to WaitGroup to perform it asynchronously, new goroutine for each dependency found
 		wg.Add(1)
 		go resolveDependencies(rootPkg, pkgVersion)
 		wg.Wait()
+
+		debugLogger.Println("Changing node names...")
+
+		for k := range copiedDeps {
+			delete(copiedDeps, k)
+		}
+		changeNodeNames(rootPkg)
 
 		stringified, err := json.MarshalIndent(rootPkg, "", "  ")
 		if err != nil {
@@ -136,6 +153,7 @@ func packageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 
+	debugLogger.Println("Writing json...")
 	// Ignoring ResponseWriter errors
 	_, _ = w.Write(toWrite)
 
@@ -148,11 +166,11 @@ func packageHandler(w http.ResponseWriter, r *http.Request) {
 // IE: need to send each package retrieval on a separate thread
 func resolveDependencies(pkg *NpmPackageVersion, versionConstraint string) {
 	// IE: signal that the goroutine is done to WaitGroup before each goroutine ends
-	// defer wg.Done()
+	defer wg.Done()
 
 	// IE: debug counter
 	goroutineCount.Add(1)
-	// debugLogger.Println("Starting goroutine", goroutineCount.GetCount())
+	debugLogger.Println("Starting goroutine", goroutineCount.GetCount())
 
 	pkgMeta, err := fetchPackageMeta(pkg.Name)
 	if err != nil {
@@ -178,60 +196,110 @@ func resolveDependencies(pkg *NpmPackageVersion, versionConstraint string) {
 	// IE: need some sort of protection against circular dependencies
 	// i.e. trucolor 4.0.4 cannot be retrieved, npmjs eventually closes the connection and sends GOAWAY
 	for dependencyName, dependencyVersionConstraint := range npmPkg.Dependencies {
-		dep := &NpmPackageVersion{Name: dependencyName, Version: dependencyVersionConstraint, Dependencies: map[string]*NpmPackageVersion{}}
 
-		// // IE: get dependencies for nodes already scanned, NOT WORKING ATM
-		var cachedDeps *NpmPackageVersion
-		getCachedDeps(dependencyName, dependencyVersionConstraint, rootPkg, cachedDeps)
-		if cachedDeps != nil {
+		// IE: get dependencies for nodes already scanned, NOT WORKING ATM
+		var cachedDeps, emptyNpmPackageVersion NpmPackageVersion
+		getCachedDeps(dependencyName, dependencyVersionConstraint, rootPkg, &cachedDeps)
+		if cachedDeps.Name != emptyNpmPackageVersion.Name && cachedDeps.Version != emptyNpmPackageVersion.Version {
 			pkg.Dependencies = cachedDeps.Dependencies
 		} else {
+			newEmptyDeps := make(map[string]*NpmPackageVersion)
+			dep := &NpmPackageVersion{Name: dependencyName, Version: dependencyVersionConstraint, Dependencies: newEmptyDeps}
 
-			pkg.Dependencies[dependencyName] = dep
+			pkg.Dependencies[uuid.NewString()] = dep
 			if len(pkg.Dependencies) > 0 {
 				// IE: send each each package dependency retrieval on a new goroutine
-				// wg.Add(1)
-				// go resolveDependencies(dep, dependencyVersionConstraint)
-
-				resolveDependencies(dep, dependencyVersionConstraint)
-
+				wg.Add(1)
+				go resolveDependencies(dep, dependencyVersionConstraint)
 			}
 
 		}
 	}
 
 	// IE: debug counter
-	// debugLogger.Println("Ending goroutine", goroutineCount.GetCount())
+	debugLogger.Println("Ending goroutine", goroutineCount.GetCount())
 	goroutineCount.Add(-1)
+
+	scannedPkgsMutex.Lock()
 	scannedPkgs[fmt.Sprintf("%s%s", pkg.Name, pkg.Version)] = pkg
+	scannedPkgsMutex.Unlock()
+
 	debugLogger.Println("Scanned package", fmt.Sprintf("%s%s", pkg.Name, pkg.Version))
 }
 
 // IE: this func should be checking asynchronously if a certain package
 // has already been scanned for dependency during current request and would return that node
 // so that its dependencies could be simply copied to another tree level where that dependency resides
-// TODO: NOT WORKING ATM, FIX IT
 func getCachedDeps(name string, version string, curNode *NpmPackageVersion, cachedDeps *NpmPackageVersion) {
-	// if len(curNode.Dependencies) == 0 {
-	// 	cachedDeps = []
-	// 	return
-	// }
+	scannedPkgsMutex.RLock()
+	cachedPkg, exist := scannedPkgs[fmt.Sprintf("%s%s", name, strings.Trim(version, "^"))]
+	scannedPkgsMutex.RUnlock()
 
-	// for _, dep := range curNode.Dependencies {
-	// 	if name == dep.Name && version == dep.Version {
-	// 		cachedDeps = dep
-	// 		debugLogger.Println("Found duplicate: ", dep.Name, dep.Version)
-	// 		return
-	// 	}
+	if exist {
+		*cachedDeps = *cachedPkg
 
-	// 	getCachedDeps(name, version, dep, cachedDeps)
-	// }
-
-	if cachedPkg, exist := scannedPkgs[fmt.Sprintf("%s%s", name, version)]; exist {
-		cachedDeps = cachedPkg
 		debugLogger.Println("Found duplicate: ", cachedPkg.Name, cachedPkg.Version)
+
 		return
 	}
+}
+
+func copyDeps(src *NpmPackageVersion, dest *NpmPackageVersion) {
+	if src.Dependencies == nil || len(src.Dependencies) == 0 {
+		return
+	}
+
+	newEmptyDeps := make(map[string]*NpmPackageVersion)
+	dest = &NpmPackageVersion{Name: src.Name, Version: src.Version, Dependencies: newEmptyDeps}
+
+	for k, dep := range src.Dependencies {
+		if dep != nil {
+			newEmptyDeps := make(map[string]*NpmPackageVersion)
+			curNode := &NpmPackageVersion{Name: dep.Name, Version: dep.Version, Dependencies: newEmptyDeps}
+			copyDeps(dep, curNode)
+			dest.Dependencies[k] = curNode
+		}
+	}
+}
+
+func changeNodeNames(node *NpmPackageVersion) {
+	if node.Dependencies == nil || len(node.Dependencies) == 0 {
+		debugLogger.Println("no more deps")
+		return
+	}
+
+	debugLogger.Println("retrieving deps for", node.Name, node.Version)
+	for nodeName, dep := range node.Dependencies {
+		node.set(uuid.NewString(), dep)
+		node.delete(nodeName)
+
+		if dep != nil {
+			if _, ok := copiedDeps[dep.Name+dep.Version]; !ok {
+				copiedDeps[dep.Name+dep.Version] = dep.Version
+				changeNodeNames(dep)
+			} else {
+				dep.Dependencies = map[string]*NpmPackageVersion{}
+			}
+		}
+	}
+}
+
+func (r *NpmPackageVersion) get(key string) *NpmPackageVersion {
+	r.RLock()
+	defer r.RUnlock()
+	return r.Dependencies[key]
+}
+
+func (r *NpmPackageVersion) set(key string, value *NpmPackageVersion) {
+	r.Lock()
+	defer r.Unlock()
+	r.Dependencies[key] = value
+}
+
+func (r *NpmPackageVersion) delete(key string) {
+	r.Lock()
+	defer r.Unlock()
+	delete(r.Dependencies, key)
 }
 
 func highestCompatibleVersion(constraintStr string, versions *npmPackageMetaResponse) (string, error) {
--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.16
 require (
 	github.com/Masterminds/semver/v3 v3.1.1
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/stretchr/testify v1.7.0
+	github.com/ulule/deepcopier v0.0.0-20200430083143-45decc6639b6 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
